c70faf66a757f26d11a615d325803e1392c5fdbd
Novachi <kubanowak9755@gmail.com>
2019-03-19 20:19:49 +0100

    started working on main view

--- rpg-helper-app/src/App.js
+++ rpg-helper-app/src/App.js
@@ -1,26 +1,59 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+
+
+class AddSession extends Component {
+  render() {
+    return (
+      <h1>Dodaj se</h1>
+    );
+  }
+}
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="App Container">
+        <div className="titleDiv">
+          <h1 id="title">RPG-Helper</h1>
+        </div>
+        <div className="controls">
+          <div className="label control">
+            <label>Choose session:</label>
+          </div>
+          <div className="control">
+            <select id="sessionsDropdown">
+              <option>1</option>
+              <option>1</option>
+              <option>1</option>
+            </select>
+          </div>
+          <div className="control">
+            <label className="button"><a href="#">Accept</a></label>
+          </div>
+          <div className="control">
+            <label className="label">Or add a new one:</label>
+          </div>
+          <div className="control">
+            <label className="button"><a href="/add/session">Add session</a></label>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" component={Main}/>
+          <Route path="/add/session" component={AddSession}/>
+        </div>
+      </Router>
     );
   }
 }
