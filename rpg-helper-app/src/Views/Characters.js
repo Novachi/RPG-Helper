6278df3188430d8aa455c2fc762ead62f3f87d3f
Novachi <kubanowak9755@gmail.com>
2019-03-27 20:35:06 +0100

    set up positioning on current views

--- rpg-helper-app/src/Views/Characters.js
+++ rpg-helper-app/src/Views/Characters.js
@@ -4,41 +4,214 @@ import React, { Component } from 'react';
 class Characters extends Component {
     render() {
       return (
-          <div className="screen">
-        <div className={"main-list-container" + " " + "column" + " " + "characterCard"}>
-                    <div className={"main-box" + " " + "top-center"}>
-                        <h1>Characters</h1>
+        <div className="topContainer">
+                <div className="centeredTopRow">
+                    <h1>Characters</h1>
+                </div>
+                <div className="centeredColumn">
+                    <div className={"centeredRow" + " " + "characterBox"}>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                    <img className="ryj" src="http://studioopinii.pl/wp-content/uploads/2013/02/glinski.jpg" width="150px" height="150px" alt="Character's ryj"/>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Name: Knauf Goldbaum</div>
+                                <div className="sectionItem">Race: Elfik</div>
+                                <div className="sectionItem">Class: Chuop</div>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Player: Theveloper</div>
+                                <div className="sectionItem">Level: 32</div>
+                                <div className="sectionItem">Goldbaum: 1000</div>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Sex: Male</div>
+                                <div className="sectionItem">Available Exp: 200</div>
+                                <div className="sectionItem"><label className="button"><a href="/characters/character">Details</a></label></div>
+                            </div> 
                     </div>
-                    <div className={"center-container"}>
-                        <div className={"main-box" + " " + "remove-padding" + " " + "row"}>
-                                <div className="column">
-                                        <img className="ryj" src="http://studioopinii.pl/wp-content/uploads/2013/02/glinski.jpg" width="200px" height="200px" alt="Character's ryj"/>
-                                </div>
-                                <div className="column">
-                                    <div className="second-box">Name: Knauf Goldbaum</div>
-                                    <div className="second-box">Race: Elfik</div>
-                                    <div className="second-box">Class: Chuop</div>
-                                </div>
-                                <div className="column">
-                                    <div className="second-box">Player: Theveloper</div>
-                                    <div className="second-box">Level: 32</div>
-                                    <div className="second-box">Goldbaum: 1000</div>
-                                </div>
-                                <div className="column">
-                                    <div className="second-box">Sex: Male</div>
-                                    <div className="second-box">Available Exp: 200</div>
-                                    <div className="second-box"><label className="button"><a href="/characters/character">Details</a></label></div>
-                                </div>
-
-                        
-                            
-                        </div>
-                    
-                            
-                    
+                    <div className={"centeredRow" + " " + "characterBox"}>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                    <img className="ryj" src="http://studioopinii.pl/wp-content/uploads/2013/02/glinski.jpg" width="150px" height="150px" alt="Character's ryj"/>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Name: Knauf Goldbaum</div>
+                                <div className="sectionItem">Race: Elfik</div>
+                                <div className="sectionItem">Class: Chuop</div>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Player: Theveloper</div>
+                                <div className="sectionItem">Level: 32</div>
+                                <div className="sectionItem">Goldbaum: 1000</div>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Sex: Male</div>
+                                <div className="sectionItem">Available Exp: 200</div>
+                                <div className="sectionItem"><label className="button"><a href="/characters/character">Details</a></label></div>
+                            </div> 
+                    </div>
+                    <div className={"centeredRow" + " " + "characterBox"}>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                    <img className="ryj" src="http://studioopinii.pl/wp-content/uploads/2013/02/glinski.jpg" width="150px" height="150px" alt="Character's ryj"/>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Name: Knauf Goldbaum</div>
+                                <div className="sectionItem">Race: Elfik</div>
+                                <div className="sectionItem">Class: Chuop</div>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Player: Theveloper</div>
+                                <div className="sectionItem">Level: 32</div>
+                                <div className="sectionItem">Goldbaum: 1000</div>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Sex: Male</div>
+                                <div className="sectionItem">Available Exp: 200</div>
+                                <div className="sectionItem"><label className="button"><a href="/characters/character">Details</a></label></div>
+                            </div> 
+                    </div>
+                    <div className={"centeredRow" + " " + "characterBox"}>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                    <img className="ryj" src="http://studioopinii.pl/wp-content/uploads/2013/02/glinski.jpg" width="150px" height="150px" alt="Character's ryj"/>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Name: Knauf Goldbaum</div>
+                                <div className="sectionItem">Race: Elfik</div>
+                                <div className="sectionItem">Class: Chuop</div>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Player: Theveloper</div>
+                                <div className="sectionItem">Level: 32</div>
+                                <div className="sectionItem">Goldbaum: 1000</div>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Sex: Male</div>
+                                <div className="sectionItem">Available Exp: 200</div>
+                                <div className="sectionItem"><label className="button"><a href="/characters/character">Details</a></label></div>
+                            </div> 
+                    </div>
+                    <div className={"centeredRow" + " " + "characterBox"}>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                    <img className="ryj" src="http://studioopinii.pl/wp-content/uploads/2013/02/glinski.jpg" width="150px" height="150px" alt="Character's ryj"/>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Name: Knauf Goldbaum</div>
+                                <div className="sectionItem">Race: Elfik</div>
+                                <div className="sectionItem">Class: Chuop</div>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Player: Theveloper</div>
+                                <div className="sectionItem">Level: 32</div>
+                                <div className="sectionItem">Goldbaum: 1000</div>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Sex: Male</div>
+                                <div className="sectionItem">Available Exp: 200</div>
+                                <div className="sectionItem"><label className="button"><a href="/characters/character">Details</a></label></div>
+                            </div> 
+                    </div>
+                    <div className={"centeredRow" + " " + "characterBox"}>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                    <img className="ryj" src="http://studioopinii.pl/wp-content/uploads/2013/02/glinski.jpg" width="150px" height="150px" alt="Character's ryj"/>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Name: Knauf Goldbaum</div>
+                                <div className="sectionItem">Race: Elfik</div>
+                                <div className="sectionItem">Class: Chuop</div>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Player: Theveloper</div>
+                                <div className="sectionItem">Level: 32</div>
+                                <div className="sectionItem">Goldbaum: 1000</div>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Sex: Male</div>
+                                <div className="sectionItem">Available Exp: 200</div>
+                                <div className="sectionItem"><label className="button"><a href="/characters/character">Details</a></label></div>
+                            </div> 
+                    </div>
+                    <div className={"centeredRow" + " " + "characterBox"}>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                    <img className="ryj" src="http://studioopinii.pl/wp-content/uploads/2013/02/glinski.jpg" width="150px" height="150px" alt="Character's ryj"/>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Name: Knauf Goldbaum</div>
+                                <div className="sectionItem">Race: Elfik</div>
+                                <div className="sectionItem">Class: Chuop</div>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Player: Theveloper</div>
+                                <div className="sectionItem">Level: 32</div>
+                                <div className="sectionItem">Goldbaum: 1000</div>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Sex: Male</div>
+                                <div className="sectionItem">Available Exp: 200</div>
+                                <div className="sectionItem"><label className="button"><a href="/characters/character">Details</a></label></div>
+                            </div> 
+                    </div>
+                    <div className={"centeredRow" + " " + "characterBox"}>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                    <img className="ryj" src="http://studioopinii.pl/wp-content/uploads/2013/02/glinski.jpg" width="150px" height="150px" alt="Character's ryj"/>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Name: Knauf Goldbaum</div>
+                                <div className="sectionItem">Race: Elfik</div>
+                                <div className="sectionItem">Class: Chuop</div>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Player: Theveloper</div>
+                                <div className="sectionItem">Level: 32</div>
+                                <div className="sectionItem">Goldbaum: 1000</div>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Sex: Male</div>
+                                <div className="sectionItem">Available Exp: 200</div>
+                                <div className="sectionItem"><label className="button"><a href="/characters/character">Details</a></label></div>
+                            </div> 
+                    </div>
+                    <div className={"centeredRow" + " " + "characterBox"}>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                    <img className="ryj" src="http://studioopinii.pl/wp-content/uploads/2013/02/glinski.jpg" width="150px" height="150px" alt="Character's ryj"/>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Name: Knauf Goldbaum</div>
+                                <div className="sectionItem">Race: Elfik</div>
+                                <div className="sectionItem">Class: Chuop</div>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Player: Theveloper</div>
+                                <div className="sectionItem">Level: 32</div>
+                                <div className="sectionItem">Goldbaum: 1000</div>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Sex: Male</div>
+                                <div className="sectionItem">Available Exp: 200</div>
+                                <div className="sectionItem"><label className="button"><a href="/characters/character">Details</a></label></div>
+                            </div> 
+                    </div>
+                    <div className={"centeredRow" + " " + "characterBox"}>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                    <img className="ryj" src="http://studioopinii.pl/wp-content/uploads/2013/02/glinski.jpg" width="150px" height="150px" alt="Character's ryj"/>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Name: Knauf Goldbaum</div>
+                                <div className="sectionItem">Race: Elfik</div>
+                                <div className="sectionItem">Class: Chuop</div>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Player: Theveloper</div>
+                                <div className="sectionItem">Level: 32</div>
+                                <div className="sectionItem">Goldbaum: 1000</div>
+                            </div>
+                            <div className={ "centeredColumn" + " " + "section"}>
+                                <div className="sectionItem">Sex: Male</div>
+                                <div className="sectionItem">Available Exp: 200</div>
+                                <div className="sectionItem"><label className="button"><a href="/characters/character">Details</a></label></div>
+                            </div> 
                     </div>
                 </div>
-                </div>
+                
+        </div>
       );
     }
   }
